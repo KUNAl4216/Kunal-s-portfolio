6403d2deb181fabac92940a422eb1960870adcbe
KUNAL RAYCHANDANI <112930829+KUNAl4216@users.noreply.github.com>
2025-03-19 12:16:13 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -91,23 +91,20 @@ export default function Navigation() {
         </div>
       </div>
 
-      {/* Mobile Navigation */}
-      {isOpen && (
-        <div className="md:hidden">
-          <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-            {navItems.map((item) => (
-              <a
-                key={item.label}
-                href={item.href}
-                className="block px-3 py-2 text-[#EAEAEA] hover:text-[#ff004f] dark:hover:text-[#00BFFF]"
-                onClick={() => setIsOpen(false)}
-              >
-                {item.label}
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
-    </nav>
-  );
-}
+     {/* Mobile Navigation */}
+{isOpen && (
+  <div className="md:hidden fixed inset-0 bg-black z-50">
+    <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      {navItems.map((item) => (
+        <a
+          key={item.label}
+          href={item.href}
+          className="block px-3 py-2 text-[#EAEAEA] hover:text-[#ff004f] dark:hover:text-[#00BFFF]"
+          onClick={() => setIsOpen(false)}
+        >
+          {item.label}
+        </a>
+      ))}
+    </div>
+  </div>
+)}
